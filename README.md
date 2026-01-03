a97e69ac439b760101ab8abe1edb4ff5f496f4b8
b <b@scrapco.de>
2026-01-03 17:24:48 +0100

    smoke test

--- README.md
+++ README.md
@@ -17,11 +17,11 @@ Ghidra must be restarted is Python dependencies are updated for the changes to t
 
 The exporter currently handles function symbols and data types (incl. function signatures).
 
-Configuration variables:
+Configuration variables (set as environment variables):
 
 * `EXPORT_PATH` - Path to the export file.
 * `LOG_FILE` - The console can't hold too much information so it's good to have an on-disk log to investigate any failed exports.
-* `LOG_LEVEL` - Log level
+* `LOG_LEVEL` - Log level (use constant names from Pythons `logging`)
 * `SOURCE_ARCHIVE_PREFIX` - Only export data types from data type archives starting with this string.
 
 
@@ -31,14 +31,42 @@ The importer currently handles data types and adds them under the "REshare" cate
 
 The importer also loads function signatures and applies them to currently defined functions with matching names.
 
-Configuration variables:
+Configuration variables (set as environment variables):
 
 * `EXPORT_PATH` - Path to the import file.
 * `LOG_FILE` - The console can't hold too much information so it's good to have an on-disk log to investigate any failed imports.
-* `LOG_LEVEL` - Log level
+* `LOG_LEVEL` - Log level (use constant names from Pythons `logging`)
 * `TYPE_IMPORT_ALLOW_RE` - Only import types that match this regular expression (`None` to disable). 
 * `TYPE_IMPORT_DENY_RE` - Don't import types that match this regular expression (`None` to disable).
 * `FUNC_SYM_IMPORT_ALLOW_RE` - Only import function signatures that match this regular expression (`None` to disable).
 * `FUNC_SYM_IMPORT_DENY_RE` - Don't import function signatures that match this regular expression (`None` to disable).
 * `FUNC_SYM_IMPORT_ADDRESS` - Import function symbols to addresses as indicated in the REshare JSON (default `False`)
+
+## Testing
+
+### Unstripping (Smoke Test)
+
+Given a binary `B` with debug symbols and its stripped version `B'` we test if `export(import(B',export(B))) == export(B)`. 
+
+Results can be manually evaluated for now, because `export(B)` and `export(B')` are bound to differ at multiple places:
+
+* Executable metadata (e.g. embedded symbols)
+* Fallback data types
+* ???
+
+A test script is provided that starts headless ghidra and runs the import and exports on a temporary project. The script must be provided with a [sample directory](https://github.com/v-p-b/reshare/tree/main/samples/minimal-gcc) where a debug and a stripped build of the same executable are present with `..._debug` and `_stripped` file name suffices:
+
+```
+$ export GHIDRA_INSTALL_DIR=/path/to/ghidra
+$ export FUNC_SYM_IMPORT_ALLOW_RE= ... # Optional configuration for importer/exporter
+$ uv run tests/unstrip.py /path/to/samples/
+[...test runs...]
+Results saved to /tmp/randompath/
+```
+
+Evaluation is facilitated by canonicalizing the JSONs as described in RFC 8785 then applying the same formatting on both exports so line-based diffing is possible.
+
+```
+$ diff /tmp/randompath/debug_canonical.json /tmp/randompath/unstripped_canonical.json
+```
 
--- test/unstrip.py
+++ test/unstrip.py
@@ -1,3 +1,4 @@
+import sys
 import pyghidra, jpype
 import tempfile
 import os
@@ -5,13 +6,19 @@ import json
 import rfc8785
 
 pyghidra.start()
-with tempfile.TemporaryDirectory() as project_dir:
+
+target_dir=sys.argv[1]
+
+with tempfile.TemporaryDirectory(delete=False) as project_dir:
+    reshare_path=os.path.join(project_dir, "reshare.json")
+    os.environ["IMPORT_PATH"]=reshare_path
+    os.environ["EXPORT_PATH"]=reshare_path
     with pyghidra.open_project(project_dir, "TestProject", create=True) as project:
         loader = pyghidra.program_loader().project(project)
         p_debug = None
         p_stripped = None
-        for f in os.listdir(os.environ["RESH_TEST_TARGET"]):
-            full_path = os.path.join(os.environ["RESH_TEST_TARGET"], f)
+        for f in os.listdir(target_dir):
+            full_path = os.path.join(target_dir, f)
             if os.path.isfile(full_path) and ("_debug" in f or "_stripped" in f):
                 if "_debug" in f:
                     assert p_debug is None
@@ -27,37 +34,40 @@ with tempfile.TemporaryDirectory() as project_dir:
             program.save("Analyzed", pyghidra.task_monitor())
             pyghidra.ghidra_script(
                 os.path.join(
-                    os.environ["RESH_DIR"], "reshare-pyghidra-export.py"
+                    os.getcwd(), "reshare-pyghidra-export.py"
                 ),
                 project,
                 program,
             )
-            json_debug = json.load(open("/tmp/reshare.json", "r"))
+            json_debug = json.load(open(reshare_path, "r"))
             with open(
-                os.path.join("/tmp", "debug_canonical.json"), "wb"
+                os.path.join(project_dir, "debug_canonical.json"), "w"
             ) as _io:
-                rfc8785.dump(json_debug, _io)
+                j=json.loads(rfc8785.dumps(json_debug))
+                json.dump(j, _io, indent=2)
         with pyghidra.program_context(project, f"/{p_stripped}") as program:
             pyghidra.analyze(program, pyghidra.task_monitor(30))
             program.save("Analyzed", pyghidra.task_monitor())
             pyghidra.ghidra_script(
                 os.path.join(
-                    os.environ["RESH_DIR"], "reshare-pyghidra-import.py"
+                    os.getcwd(), "reshare-pyghidra-import.py"
                 ),
                 project,
                 program,
             )
             pyghidra.ghidra_script(
                 os.path.join(
-                    os.environ["RESH_DIR"], "reshare-pyghidra-export.py"
+                    os.getcwd(), "reshare-pyghidra-export.py"
                 ),
                 project,
                 program,
             )
-            json_stripped = json.load(open("/tmp/reshare.json", "r"))
+            json_stripped = json.load(open(reshare_path, "r"))
             with open(
-                os.path.join("/tmp", "unstripped_canonical.json"), "wb"
+                os.path.join(project_dir, "unstripped_canonical.json"), "w"
             ) as _io:
-                rfc8785.dump(json_stripped, _io)
-        print(project_dir)
-exit(0)
+                j=json.loads(rfc8785.dumps(json_stripped))
+                json.dump(j, _io, indent=2)
+        del os.environ["IMPORT_PATH"]
+        del os.environ["EXPORT_PATH"]
+        print(f"Results saved to {project_dir}")
